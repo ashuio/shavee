f2975a95213c0b0d759f706662213782bcd6b45d
Kiavash <46830209+kiavash-at-work@users.noreply.github.com>
2021-08-13 13:15:22 -0700

    Single point of entry and exit
    `main.rs` updated to
    * `main()` only implements entry and exit code, the rest is moved to `run()`.
       * This change make it easier to implement Integration Test.
       * Refactor all the error message into a single function.
    * implemented `enum` instead of `String`

--- shavee-bin/src/main.rs
+++ shavee-bin/src/main.rs
@@ -1,90 +1,54 @@
 mod args;
 
-use args::Sargs;
+use args::{Sargs, Mode, Umode};
 use base64::encode_config;
 use shavee_lib::logic::{create_zfs_file, create_zfs_yubi, unlock_zfs_pass};
 use shavee_lib::logic::{print_mode_file, print_mode_yubi, unlock_zfs_file, unlock_zfs_yubi};
 use shavee_lib::password::hash_argon2;
 use shavee_lib::zfs::*;
+use std::error::Error;
 use std::process::exit;
 
+// main() collect the arguments from command line, pass them to run() and print any error 
+// message upon exiting the program
 fn main() {
     let args = Sargs::new();
-    let pass = rpassword::prompt_password_stderr("Dataset Password: ");
-    let pass = match pass {
-        Ok(pass) => pass,
-        Err(error) => {
-            eprintln!("Error: Failed to read Password");
-            eprintln!("Error: {}", error);
-            exit(1)
-        }
-    };
 
-    match args.umode.as_str() {
-        "yubikey" => match args.mode.as_str() {
-            "print" => match print_mode_yubi(pass, args.yslot) {
-                Ok(()) => exit(0),
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                    exit(1)
-                }
-            },
-            "pam" | "mount" => match unlock_zfs_yubi(pass, args.dataset, args.yslot) {
-                Ok(()) => exit(0),
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                    exit(1)
-                }
-            },
-            "create" => match create_zfs_yubi(pass, args.dataset, args.yslot) {
-                Ok(()) => exit(0),
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                    exit(1)
-                }
-            },
-            _ => unreachable!(),
+    // Only main() will terminate the executable with proper error message and code
+    exit(match run(args){
+        Ok(()) => 0,
+        Err(error) => {
+            eprintln!("Error: {}", error);
+            1
+        }
+    });
+}
+
+
+fn run(args: Sargs) -> Result<(), Box<dyn Error>> {
+
+    let pass = rpassword::prompt_password_stderr("Dataset Password: ")
+        .map_err(|e| e.to_string())?;
+
+    Ok(match args.umode {
+        Umode::Yubikey => match args.mode {
+            Mode::Print => print_mode_yubi(pass, args.yslot)?,
+            Mode::Mount => unlock_zfs_yubi(pass, args.dataset, args.yslot)?,
+            Mode::Create => create_zfs_yubi(pass, args.dataset, args.yslot)?,
         },
-        "file" => match args.mode.as_str() {
-            "print" => match print_mode_file(pass, &args.file, args.port) {
-                Ok(()) => exit(0),
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                    exit(1)
-                }
-            },
-            "pam" | "mount" => match unlock_zfs_file(pass, args.file, args.dataset, args.port) {
-                Ok(()) => exit(0),
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                    exit(1)
-                }
-            },
-            "create" => match create_zfs_file(pass, args.file, args.dataset, args.port) {
-                Ok(()) => exit(0),
-                Err(e) => {
-                    eprintln!("Error: {}", e);
-                    exit(1)
-                }
-            },
-            _ => unreachable!(),
+        Umode::File => match args.mode {
+            Mode::Print => print_mode_file(pass, args.file, args.port)?,
+            Mode::Mount => unlock_zfs_file(pass, args.file, args.dataset, args.port)?,
+            Mode::Create => create_zfs_file(pass, args.file, args.dataset, args.port)?,
         },
-        "password" => {
-            let key = hash_argon2(pass.into_bytes()).unwrap();
+        Umode::Password => {
+            let key =  hash_argon2(pass.into_bytes())?;
             let key = encode_config(key, base64::STANDARD_NO_PAD);
-            match args.mode.as_str() {
-                "print" => println!("{}", key),
-                "pam" | "mount" => unlock_zfs_pass(key, args.dataset).unwrap(),
-                "create" => match zfs_create(key, args.dataset) {
-                    Ok(()) => (),
-                    Err(e) => {
-                        eprintln!("Error: {}", e);
-                        exit(1)
-                    }
-                },
-                _ => unreachable!(),
+            match args.mode {
+                Mode::Print => println!("{}", key),
+                Mode::Mount => unlock_zfs_pass(key, args.dataset)?,
+                Mode::Create => zfs_create(key, args.dataset)?,
             }
         }
-        _ => unreachable!(),
-    }
+    })
 }
