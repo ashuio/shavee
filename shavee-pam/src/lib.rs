97eb9fca173fc648ac2c9573ab61fa81016071a1
Kiavash <46830209+kiavash-at-work@users.noreply.github.com>
2021-09-03 13:02:58 -0700

    Verbose error reporting

--- shavee-pam/src/lib.rs
+++ shavee-pam/src/lib.rs
@@ -19,85 +19,56 @@ impl PamServiceModule for PamShavee {
     }
 
     fn close_session(pam: Pam, _flags: PamFlags, args: Vec<String>) -> PamError {
-        let state = match pam_parse_args(args) {
+        let (_umode, dataset, _pass) = match parse_pam_args(args, pam) {
             Ok(value) => value,
             Err(e) => return e,
         };
 
-        let dataset =
-            match pam_user_pass_expect(pam.get_user(Some("Username: ")), PamError::USER_UNKNOWN) {
-                Ok(user) => {
-                    let mut d = state.dataset;
-                    d.push('/');
-                    d.push_str(user);
-                    d
-                }
-                Err(e) => return e,
-            };
-
-        match zfs_umount(dataset) {
+        match zfs_umount(dataset.clone()) {
             Ok(()) => return PamError::SUCCESS,
-            Err(_) => return PamError::SESSION_ERR,
+            Err(e) => {
+                eprintln!(
+                    "Error in unmounting user ZFS {} dataset: {}",
+                    dataset,
+                    e.to_string()
+                );
+                return PamError::SESSION_ERR;
+            }
         }
     }
+
     fn authenticate(pam: Pam, _flags: PamFlags, args: Vec<String>) -> PamError {
-        let state = match pam_parse_args(args) {
+        let (umode, dataset, pass) = match parse_pam_args(args, pam) {
             Ok(value) => value,
             Err(e) => return e,
         };
 
-        let dataset =
-            match pam_user_pass_expect(pam.get_user(Some("Username: ")), PamError::USER_UNKNOWN) {
-                Ok(user) => {
-                    let mut d = state.dataset;
-                    d.push('/');
-                    d.push_str(user);
-                    d
-                }
-                Err(e) => return e,
-            };
+        let result = match umode {
+            shavee_lib::Umode::Yubikey { yslot } => unlock_zfs_yubi(pass, Some(dataset), yslot),
 
-        let pass = match pam_user_pass_expect(pam.get_authtok(None), PamError::AUTHINFO_UNAVAIL) {
-            Ok(value) => value.to_string(),
-            Err(value) => return value,
-        };
-
-        match state.umode {
-            shavee_lib::Umode::Yubikey { yslot } => {
-                match unlock_zfs_yubi(pass, Some(dataset), yslot) {
-                    Ok(_) => return PamError::SUCCESS,
-                    Err(e) => {
-                        eprintln!("Error: {}", e.to_string());
-                        return PamError::AUTH_ERR;
-                    }
-                }
-            }
             shavee_lib::Umode::File { file, port, size } => {
                 let filehash = match get_filehash(file, port, size) {
                     Ok(e) => e,
                     Err(e) => {
-                        eprintln!("Error: {}", e.to_string());
+                        eprintln!("Error in generating filehash: {}", e.to_string());
                         return PamError::AUTHINFO_UNAVAIL;
                     }
                 };
-                match unlock_zfs_file(pass, filehash, Some(dataset)) {
-                    Ok(_) => return PamError::SUCCESS,
-                    Err(e) => {
-                        eprintln!("Error: {}", e.to_string());
-                        return PamError::AUTH_ERR;
-                    }
-                }
+                unlock_zfs_file(pass, filehash, Some(dataset))
             }
+
             shavee_lib::Umode::Password => {
                 let key = hash_argon2(pass.into_bytes()).unwrap();
                 let key = encode_config(key, base64::STANDARD_NO_PAD);
-                match unlock_zfs_pass(key, Some(dataset)) {
-                    Ok(_) => return PamError::SUCCESS,
-                    Err(e) => {
-                        eprintln!("Error: {}", e.to_string());
-                        return PamError::AUTH_ERR;
-                    }
-                }
+                unlock_zfs_pass(key, Some(dataset))
+            }
+        };
+
+        match result {
+            Ok(_) => return PamError::SUCCESS,
+            Err(e) => {
+                eprintln!("Error in mounting user ZFS dataset: {}", e.to_string());
+                return PamError::AUTH_ERR;
             }
         }
     }
@@ -107,19 +78,42 @@ impl PamServiceModule for PamShavee {
     }
 }
 
-fn pam_parse_args(args: Vec<String>) -> Result<args::Pargs, PamError> {
-    let mut clap_args: Vec<String> = Vec::new();
-    clap_args.push("libshavee_pam.so".to_string());
-    clap_args.extend(args);
-    let state = match args::Pargs::new_from(clap_args.into_iter()) {
-        // Parse Args
-        Ok(args) => args,
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            return Err(PamError::BAD_ITEM);
-        }
+fn parse_pam_args(
+    args: Vec<String>,
+    pam: Pam,
+) -> Result<(shavee_lib::Umode, String, String), PamError> {
+    let state = match {
+        let mut clap_args: Vec<String> = Vec::new();
+        clap_args.push("libshavee_pam.so".to_string());
+        clap_args.extend(args);
+        let state = match args::Pargs::new_from(clap_args.into_iter()) {
+            // Parse Args
+            Ok(args) => args,
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                return Err(PamError::BAD_ITEM);
+            }
+        };
+        Ok(state)
+    } {
+        Ok(value) => value,
+        Err(e) => return Err(e),
     };
-    Ok(state)
+    let dataset =
+        match pam_user_pass_expect(pam.get_user(Some("Username: ")), PamError::USER_UNKNOWN) {
+            Ok(user) => {
+                let mut d = state.dataset;
+                d.push('/');
+                d.push_str(user);
+                d
+            }
+            Err(e) => return Err(e),
+        };
+    let pass = match pam_user_pass_expect(pam.get_authtok(None), PamError::AUTHINFO_UNAVAIL) {
+        Ok(value) => value.to_string(),
+        Err(value) => return Err(value),
+    };
+    Ok((state.umode, dataset, pass))
 }
 
 fn pam_user_pass_expect(
