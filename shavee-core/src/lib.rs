8bc25ff1d128c84f4a09edf3fc3180d6172332fb
Kiavash <46830209+kiavash-at-work@users.noreply.github.com>
2022-02-04 14:51:57 -0800

    closed TODO
    * unit test
    * clap2 deprecated function replaced with clap3

--- shavee-core/src/lib.rs
+++ shavee-core/src/lib.rs
@@ -9,44 +9,52 @@ pub const UNREACHABLE_CODE: &str =
 
 use clap;
 
-// TODO: Write unit test
 pub fn parse_file_size_arguments(
-    values: clap::Values,
+    file_size_argument: Vec<&str>,
 ) -> Result<(Option<String>, Option<u64>), clap::Error> {
-    // initiate size to None. If user entered SIZE arg value, then will fill it with Some()
-    let mut size = None;
-
-    // convert the values to a vector
-    let file_size_argument: Vec<_> = values.collect();
+    // If user entered SIZE arg value, it will be wrapped with Some(), otherwise None will be returned
+    let size: Option<u64>;
 
     // first [0] value is the file name
     // it is a required field for "--file" and its existence already checked by clap
     let file = Some(file_size_argument[0].to_string());
 
-    // if "--file" has two entries, then 2nd [1] is size
-    if file_size_argument.len() == 2 {
-        let second_entry = file_size_argument[1];
+    match file_size_argument.len() {
+        // if there is only 1 entry then it is file name and size is set to None
+        number_of_entries if number_of_entries == 1 => {
+            size = None;
+        }
+        // if there are 2 entries, then 2nd entry is size
+        number_of_entries if number_of_entries == 2 => {
+            // if "--file" has two entries, then 2nd [1] is size
+            let second_entry = file_size_argument[1];
 
-        // however the size entry needs to be validated and return error if it is not a u64 value
-        let size_check = match second_entry.parse::<u64>() {
-            Err(_) => {
-                let error_message =
-                    format!(r#""{}" is not valid for SIZE argument."#, second_entry);
+            // however the size entry needs to be validated and return error if it is not a u64 value
+            size = match second_entry.parse::<u64>() {
+                Err(_) => {
+                    let error_message =
+                        format!(r#""{}" is not valid for SIZE argument."#, second_entry);
 
-                //TODO: "with_description" is depreciated.
-                #[allow(deprecated)]
-                return Err(clap::Error::with_description(
-                    error_message.to_string(),
-                    clap::ErrorKind::InvalidValue,
-                ));
-            }
-            Ok(u) => u,
-        };
+                    return Err(clap::Error::raw(
+                        clap::ErrorKind::InvalidValue,
+                        &error_message[..],
+                    ));
+                }
+                // wrap the parsed entry with Some()
+                Ok(size_arg) => Some(size_arg),
+            };
+        }
 
-        // wrap the parsed entry with Some()
-        size = Some(size_check);
-    }
+        // clap checks against number of entries must not allow any other value than 1 and 2 entries
+        _ => {
+            return Err(clap::Error::raw(
+                clap::ErrorKind::InvalidValue,
+                UNREACHABLE_CODE,
+            ));
+        }
+    };
 
+    // Function Return values wrap with OK indicating no error
     Ok((file, size))
 }
 
@@ -87,5 +95,32 @@ mod tests {
                 Err(_) => continue,
             }
         }
+    }
+
+    #[test]
+    fn parse_file_size_arguments_test() {
+        // test for when both file and size is provided
+        assert_eq!(
+            parse_file_size_arguments(vec!["./shavee", "2048"]).unwrap(),
+            (Some(String::from("./shavee")), Some(2048 as u64))
+        );
+
+        // test for when only file is provided
+        assert_eq!(
+            parse_file_size_arguments(vec!["./shavee"]).unwrap(),
+            (Some(String::from("./shavee")), None)
+        );
+
+        // test for when there is an empty input
+        assert_eq!(
+            parse_file_size_arguments(vec![""]).unwrap(),
+            (Some(String::from("")), None)
+        );
+
+        // test for reporting error when size is invalid
+        parse_file_size_arguments(vec!["./shavee", "ten"]).unwrap_err();
+
+        // test for reporting error when more than 2 entries are provided
+        parse_file_size_arguments(vec!["./shavee", "2048", "ten"]).unwrap_err();
     }
 }
