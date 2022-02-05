505db900fe603285f1cd6b9cc0edfb2cbfa5adee
Kiavash <46830209+kiavash-at-work@users.noreply.github.com>
2022-02-04 16:09:47 -0800

    unit test

--- shavee-core/src/password.rs
+++ shavee-core/src/password.rs
@@ -19,3 +19,46 @@ pub fn hash_argon2(password: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
         Err(e) => return Err(e.into()),
     }
 }
+
+mod tests {
+    #[test]
+    fn test_hash_argon2() {
+        use super::*;
+        // defining a struct that will hold intput arguments
+        // and their output hash results
+        // need to use io::ErrorKind (instead of io:Error) which supports PartialEq
+        #[derive(Debug, PartialEq)]
+        struct PasswordHashPair<'a> {
+            password: &'a str,
+            hash_result: Vec<u8>,
+        }
+
+        // each entry of the array holds the input/output struct
+        let password_hash_pairs = [
+            PasswordHashPair {
+                password: "",
+                hash_result: vec![
+                    13, 58, 236, 241, 152, 183, 100, 212, 216, 84, 90, 94, 168, 228, 31, 30, 77,
+                    49, 66, 19, 123, 152, 12, 239, 137, 235, 105, 65, 204, 16, 29, 214, 212, 15,
+                    173, 80, 27, 108, 127, 193, 196, 252, 102, 37, 234, 173, 71, 28, 14, 157, 76,
+                    244, 99, 170, 151, 224, 154, 190, 53, 226, 85, 233, 245, 225,
+                ],
+            },
+            PasswordHashPair {
+                password: "This is a test!",
+                hash_result: vec![
+                    85, 96, 224, 183, 7, 56, 237, 97, 204, 74, 197, 74, 92, 189, 56, 227, 126, 87,
+                    228, 28, 207, 173, 5, 111, 52, 228, 92, 208, 80, 152, 167, 236, 7, 214, 53,
+                    103, 36, 168, 140, 9, 151, 221, 179, 68, 64, 180, 176, 81, 253, 221, 210, 100,
+                    153, 141, 80, 152, 133, 23, 75, 217, 214, 153, 56, 173,
+                ],
+            },
+        ];
+
+        for index in 0..password_hash_pairs.len() {
+            if let Ok(hash) = hash_argon2(password_hash_pairs[index].password.into()) {
+                assert_eq!(hash, password_hash_pairs[index].hash_result)
+            };
+        }
+    }
+}
