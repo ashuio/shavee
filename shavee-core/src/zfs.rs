b8c6bd5e25cb1f1d24dc99127c6c2b26d163559e
Kiavash <46830209+kiavash-at-work@users.noreply.github.com>
2022-02-11 15:16:56 -0800

    unit tests for more functions

--- shavee-core/src/zfs.rs
+++ shavee-core/src/zfs.rs
@@ -1,6 +1,7 @@
-use std::error::Error;
 use std::io::prelude::*;
 use std::process::Command;
+
+use crate::UNREACHABLE_CODE;
 
 pub fn zfs_loadkey(key: String, dataset: String) -> std::io::Result<()> {
     let mut zfs = Command::new("zfs") // Call zfs mount
@@ -14,24 +15,17 @@ pub fn zfs_loadkey(key: String, dataset: String) -> std::io::Result<()> {
 
     let zstdin = zfs
         .stdin // Supply encryption key via stdin
-        .as_mut();
-
-    let zstdin = match zstdin {
-        Some(i) => i,
-        None => {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::BrokenPipe,
-                "Failed to lock stdin",
-            ))
-        }
-    };
+        .as_mut()
+        .ok_or(std::io::Error::new(
+            //convert None to an error
+            std::io::ErrorKind::BrokenPipe,
+            "Failed to lock stdin!",
+        ))?;
 
     zstdin.write_all(&key.as_bytes())?;
 
     let result = zfs.wait()?;
-    if result.success() {
-        return Ok(());
-    } else {
+    if !result.success() {
         let mut e: Vec<u8> = Vec::new();
         zfs.stderr.unwrap().read_to_end(&mut e).unwrap();
         return Err(std::io::Error::new(
@@ -39,9 +33,10 @@ pub fn zfs_loadkey(key: String, dataset: String) -> std::io::Result<()> {
             String::from_utf8_lossy(&e),
         ));
     }
+    Ok(())
 }
 
-pub fn zfs_list(dataset: String) -> Result<Vec<String>, Box<dyn Error>> {
+pub fn zfs_list(dataset: String) -> Result<Vec<String>, std::io::Error> {
     let zfs_list = Command::new("zfs")
         .arg("list")
         .arg("-H")
@@ -57,103 +52,94 @@ pub fn zfs_list(dataset: String) -> Result<Vec<String>, Box<dyn Error>> {
             std::io::ErrorKind::Other,
             //stderr used to generate the error message.
             String::from_utf8_lossy(&zfs_list.stderr).to_string(),
-        )
-        .into());
+        ));
     };
 
-    let out = String::from_utf8(zfs_list.stdout)?;
+    let out = String::from_utf8(zfs_list.stdout).map_err(|error| {
+        std::io::Error::new(
+            // error kind is not known
+            std::io::ErrorKind::InvalidInput,
+            error,
+        )
+    })?;
 
-    let list = out.split_whitespace();
-    let mut dlist: Vec<String> = Vec::new();
-
-    for i in list {
-        dlist.push(i.to_string());
-    }
-
-    return Ok(dlist);
+    let list: Vec<String> = out.split_whitespace().map(|s| s.to_string()).collect();
+    Ok(list)
 }
 
-pub fn zfs_create(key: String, dataset: Option<String>) -> std::io::Result<()> {
-    let dataset = dataset.expect(crate::UNREACHABLE_CODE);
-    let zfs_list = Command::new("zfs")
-        .arg("list")
-        .arg("-H")
-        .arg("-o")
-        .arg("name")
-        .arg("-r")
-        .arg(&dataset)
-        .output()?;
+pub fn zfs_create(key: String, dataset: String) -> std::io::Result<()> {
+    // check if dataset already exists
+    match zfs_list(dataset.clone()) {
+        // if it exists, then change password
+        Ok(list) => {
+            for i in list {
+                let mut zfs_changekey = Command::new("zfs")
+                    .arg("change-key")
+                    .arg("-o")
+                    .arg("keylocation=prompt")
+                    .arg("-o")
+                    .arg("keyformat=passphrase")
+                    .arg(&i)
+                    .stdin(std::process::Stdio::piped())
+                    .stderr(std::process::Stdio::piped())
+                    .spawn()?;
 
-    if zfs_list.status.success() {
-        let list = String::from_utf8_lossy(&zfs_list.stdout);
-        let list = list.split_whitespace();
-        for i in list {
-            let mut zfs_changekey = Command::new("zfs")
-                .arg("change-key")
-                .arg("-o")
-                .arg("keylocation=prompt")
-                .arg("-o")
-                .arg("keyformat=passphrase")
-                .arg(&i)
-                .stdin(std::process::Stdio::piped())
-                .spawn()?;
+                let zstdin = zfs_changekey
+                    .stdin // Supply encryption key via stdin
+                    .as_mut()
+                    .ok_or(
+                        // convert None to an error
+                        std::io::Error::new(
+                            std::io::ErrorKind::BrokenPipe,
+                            "Failed to get ZFS stdin!",
+                        ),
+                    )?;
 
-            let zstdin = zfs_changekey
-                .stdin // Supply encryption key via stdin
-                .as_mut();
+                zstdin.write_all(&key.as_bytes())?;
 
-            let zstdin = match zstdin {
-                Some(i) => i,
-                None => {
+                // capture the error message and pass it to the calling function
+                let result = zfs_changekey.wait_with_output()?;
+                if !result.status.success() {
                     return Err(std::io::Error::new(
-                        std::io::ErrorKind::BrokenPipe,
-                        "Failed to get ZFS stdin!",
-                    ))
+                        std::io::ErrorKind::Unsupported,
+                        String::from_utf8(result.stderr).expect(UNREACHABLE_CODE),
+                    ));
                 }
-            };
-
-            zstdin.write_all(&key.as_bytes())?;
-
-            let result = zfs_changekey.wait()?;
-            if !result.success() {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    "Failed to change key!",
-                ));
             }
         }
-    } else {
-        let mut zfs = Command::new("zfs") // Call zfs create
-            .arg("create")
-            .arg("-o")
-            .arg("encryption=on")
-            .arg("-o")
-            .arg("keyformat=passphrase")
-            .arg("-o")
-            .arg("keylocation=prompt")
-            .arg(&dataset)
-            .stdin(std::process::Stdio::piped())
-            .stderr(std::process::Stdio::piped())
-            .spawn()?;
+        // if dataset doesn't exists then create it
+        Err(_) => {
+            let mut zfs = Command::new("zfs") // Call zfs create
+                .arg("create")
+                .arg("-o")
+                .arg("encryption=on")
+                .arg("-o")
+                .arg("keyformat=passphrase")
+                .arg("-o")
+                .arg("keylocation=prompt")
+                .arg(&dataset)
+                .stdin(std::process::Stdio::piped())
+                .stderr(std::process::Stdio::piped())
+                .spawn()?;
 
-        zfs.stdin // Supply encryption key via stdin
-            .as_mut()
-            .expect("failed to get zfs stdin")
-            .write_all(&key.as_bytes())
-            .expect("Failed to write to stdin");
+            zfs.stdin // Supply encryption key via stdin
+                .as_mut()
+                .expect("failed to get zfs stdin!")
+                .write_all(&key.as_bytes())
+                .expect("Failed to write to stdin!");
 
-        let result = zfs.wait()?;
-        if !result.success() {
-            let mut e: Vec<u8> = Vec::new();
-            zfs.stderr
-                .expect("Failed to read stderr!")
-                .read_to_end(&mut e)
-                .expect("Failed to read from stderr!");
-            let error = String::from_utf8_lossy(&e);
-            return Err(std::io::Error::new(std::io::ErrorKind::BrokenPipe, error));
+            let result = zfs.wait()?;
+            if !result.success() {
+                let mut e: Vec<u8> = Vec::new();
+                zfs.stderr
+                    .expect("Failed to read stderr!")
+                    .read_to_end(&mut e)
+                    .expect("Failed to read from stderr!");
+                let error = String::from_utf8_lossy(&e);
+                return Err(std::io::Error::new(std::io::ErrorKind::BrokenPipe, error));
+            }
         }
     };
-
     Ok(())
 }
 
@@ -191,70 +177,76 @@ fn execute_zfs_subcommand_then_collect_result(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::PathBuf;
 
     #[test]
-    fn zfs_functional_test() {
-        /*
-        This unit test, will need to setup a temp ZFS Pool
-        to test the functions on it.
-        It can only work if ZFS and ZPOOL are installed and root permission is given.
-        */
+    fn zfs_list_test() {
+        // generate a temp zpool
+        let (zpool_name, temp_folder) = prepare_zpool();
+        // b/c of zpool creation process, the dataset name is the same as zpool
+        let zfs_dataset = zpool_name.clone();
 
-        // Check for root permission and exit early
-        if !nix::unistd::Uid::effective().is_root() {
-            panic!("Root permission is needed! Test terminated early!");
+        // NOTE: the output will be evaluated **AFTER** clean up so the temp folder and
+        // temp zpool can safely be removed.
+
+        let test_output = zfs_list(zfs_dataset.clone());
+
+        //clean up
+        cleanup_zpool(zpool_name, temp_folder);
+
+        // test the output against expected result.
+        match test_output {
+            Ok(result) => assert_eq!(result, vec![zfs_dataset]),
+            Err(error) => panic!("Test failed: {:?}", error),
+        }
+    }
+
+    #[test]
+    fn zfs_create_test() {
+        // generate a temp zpool
+        let (zpool_name, temp_folder) = prepare_zpool();
+
+        // b/c of zpool creation process, the dataset name is the same as zpool
+        let zfs_dataset_unencrypted = zpool_name.clone();
+
+        // NOTE: the output will be evaluated **AFTER** clean up so the temp folder and
+        // temp zpool can safely be removed.
+
+        // **TEST 1 **: zfs_create on a dataset that is not encrypted
+        // This test is expected to fail
+        let test_dataset_not_encrypted_must_fail_output =
+            zfs_create(random_string(3), zfs_dataset_unencrypted.clone());
+
+        // **TEST 2 **: zfs_create a new encrypted dataset
+        let mut zfs_encrypted_dataset = zpool_name.clone();
+        zfs_encrypted_dataset.push('/');
+        zfs_encrypted_dataset.push_str(&random_string(3));
+        let test_create_new_encrypted_dataset_output =
+            zfs_create(random_string(8), zfs_encrypted_dataset.clone()); // min accepted key is 8 character
+
+        // **TEST 3 **: zfs_create on an already encrypted dataset
+        let test_already_encrypted_dataset_output =
+            zfs_create(random_string(8), zfs_encrypted_dataset); // min accepted key is 8 character
+
+        //clean up
+        cleanup_zpool(zpool_name, temp_folder);
+
+        // now it is time to test the outputs against expected results.
+
+        // **TEST 1 **: zfs_create on a dataset that is not encrypted
+        match test_dataset_not_encrypted_must_fail_output {
+            Ok(_) => panic!("Unit test failed!"),
+            Err(error) => assert_eq!(
+                "Key change error: Dataset not encrypted.\n",
+                error.to_string()
+            ),
         }
 
-        // Check for ZFS tools and exit early
-        Command::new("zpool")
-            .spawn()
-            .expect("ZFS and ZPOOL tools must be installed. Test terminated early!");
+        // **TEST 2 **: zfs_create a new encrypted dataset
+        test_create_new_encrypted_dataset_output.expect("Unit test failed!");
 
-        // make a temp folder in the system temp directory
-        // this temp folder will be automatically deleted at the end of unit test
-        let temp_folder = tempfile::tempdir()
-            .expect("Couldn't make a temp folder! Test terminated early!")
-            .into_path();
-
-        // Use a random name to avoid modifying an existing ZFS pool and dataset
-        let zpool_name = random_string(30);
-
-        // Use a random mount point for Zpool alt_root
-        let mut zpool_alt_root = temp_folder.clone();
-        zpool_alt_root.push(random_string(10));
-
-        let mut zpool_path = temp_folder.clone();
-        zpool_path.push(zpool_name.clone());
-
-        // Generate a 512MB file which will be used as ZFS pool vdev
-        Command::new("truncate")
-            .arg("--size")
-            .arg("512M")
-            .arg(zpool_path.clone())
-            .spawn()
-            .expect("Cannot generate a temp file. Test terminated early!");
-
-        // create a new temporarily zpool using vdev
-        let command_output = Command::new("zpool")
-            .arg("create")
-            .arg(zpool_name)
-            .arg(zpool_path)
-            .arg("-R")
-            .arg(zpool_alt_root)
-            .status()
-            .expect("Zpool creation failed! Test terminated early!");
-
-        if !command_output.success() {
-            panic!("Zpool creation failed! Test terminated early!");
-        }
-
-        // now we can test the functions
-        // test creating an encrypted dataset
-        // use a random dataset and key
-        let zfs_dataset = random_string(10);
-        let zfs_key = random_string(10);
-
-        zfs_create(zfs_key, Some(zfs_dataset)).expect("Test failed: zfs_create()");
+        // **TEST 3 **: zfs_create on an already encrypted dataset
+        test_already_encrypted_dataset_output.expect("Unit test failed!");
     }
 
     //These tests checks for a reported error on non-existing dataset
@@ -304,13 +296,82 @@ mod tests {
     }
 
     fn random_string(length: u8) -> String {
-        use rand::distributions::Alphanumeric;
-        use rand::{thread_rng, Rng};
+        use random_string::generate;
 
-        thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(length as usize)
-            .map(char::from)
-            .collect()
+        // for maximum compatibility limit the random character to ASCII alphabets
+        let charset: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        generate(length as usize, charset)
+    }
+
+    // this function generates a temp folder and a zpool with a random name.
+    fn prepare_zpool() -> (String, PathBuf) {
+        // For ZFS related unit tests need root permission
+        // Check for root permission and exit early
+        if !nix::unistd::Uid::effective().is_root() {
+            panic!("Root permission is needed! Test terminated early!");
+        }
+
+        // Check for ZFS tools and exit early
+        Command::new("zpool")
+            .arg("version")
+            .spawn()
+            .expect("ZFS and ZPOOL tools must be installed. Test terminated early!");
+
+        // make a temp folder in the system temp directory
+        // this temp folder will be automatically deleted at the end of unit test
+        let temp_folder = tempfile::tempdir()
+            .expect("Couldn't make a temp folder! Test terminated early!")
+            .into_path();
+
+        // Use a random name to avoid modifying an existing ZFS pool and dataset
+        let zpool_name = random_string(30);
+
+        // Use a random mount point for Zpool alt_root
+        let mut zpool_alt_root = temp_folder.clone();
+        zpool_alt_root.push(random_string(5));
+
+        let mut zpool_path = temp_folder.clone();
+        zpool_path.push(zpool_name.clone());
+
+        // Generate a 512MB file which will be used as ZFS pool vdev
+        Command::new("truncate")
+            .arg("--size")
+            .arg("512M")
+            .arg(zpool_path.clone())
+            .spawn()
+            .expect("Cannot generate a temp file. Test terminated early!");
+
+        // create a new temporarily zpool using vdev
+        let command_output = Command::new("zpool")
+            .arg("create")
+            .arg(zpool_name.clone())
+            .arg(zpool_path.clone())
+            .arg("-R")
+            .arg(zpool_alt_root)
+            .status()
+            .expect("Zpool creation failed! Test terminated early!");
+
+        if !command_output.success() {
+            panic!("Zpool creation failed! Test terminated early!");
+        }
+
+        (zpool_name, temp_folder)
+    }
+
+    // this function removes zpool and cleans up the temp folder.
+    fn cleanup_zpool(zpool_name: String, temp_folder: PathBuf) {
+        let command_output = Command::new("zpool")
+            .arg("export")
+            .arg(zpool_name)
+            .status()
+            .expect("Failed to export Zpool!");
+        if !command_output.success() {
+            panic!("Failed to export Zpool!");
+        }
+        Command::new("rm")
+            .arg("-rf")
+            .arg(temp_folder)
+            .spawn()
+            .expect("Temp folder clean up failed!");
     }
 }
